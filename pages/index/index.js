c2b668625fc2019f4b242b54da31c4df0929a411
zhaoqi <2022145499@qq.com>
2018-05-26 14:14:18 +0800

    给每张图片底部配上说明文字

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,14 +3,21 @@
 var app = getApp()
 Page({
   data: {
-    curIndex: 2,  //当前显示的view下标
-    prev:1,
-    next:3,
-    swiperList: [1, 2, 3, 4, 5], //轮播数据列表
+    team_curIndex: 2,  //当前显示的view下标
+    team_prev:1,
+    team_next:3,
+    
+    swiperList: [
+      { 'person_src': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1527324788361&di=a7760b6d3349b4d782df4d5c59b3ead3&imgtype=0&src=http%3A%2F%2Fimg03.tooopen.com%2Fuploadfile%2Fdowns%2Fimages%2F20110714%2Fsy_20110714135215645030.jpg', 'intro_src': 'http://img3.imgtn.bdimg.com/it/u=2358689708,1558820&fm=214&gp=0.jpg' },
+      { 'person_src': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1527324788349&di=70d17984c39bbd553bb95c504444ba51&imgtype=0&src=http%3A%2F%2Fpic36.nipic.com%2F20131213%2F6704106_223232205107_2.png', 'intro_src': 'http://imgsrc.baidu.com/image/c0%3Dshijue1%2C0%2C0%2C294%2C40/sign=468315030324ab18f41be9745d938cb8/962bd40735fae6cd6f6c5b4405b30f2442a70f3e.jpg' },
+      { 'person_src': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1527324788360&di=c950b06bd7dd5f72a3290d5c898bb860&imgtype=0&src=http%3A%2F%2Fpic41.nipic.com%2F20140507%2F18602184_142854861000_2.jpg', 'intro_src': 'http://img3.imgtn.bdimg.com/it/u=2358689708,1558820&fm=214&gp=0.jpg' },
+      { 'person_src': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1527324788357&di=db45b1affb62ac9fabf70e119cdc150c&imgtype=0&src=http%3A%2F%2Fimg05.tooopen.com%2Fimages%2F20140328%2Fsy_57865838889.jpg', 'intro_src': 'http://imgsrc.baidu.com/image/c0%3Dshijue1%2C0%2C0%2C294%2C40/sign=468315030324ab18f41be9745d938cb8/962bd40735fae6cd6f6c5b4405b30f2442a70f3e.jpg' },
+      { 'person_src': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1527324788351&di=a955634d047b94386264e07cc1918cae&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F13%2F68%2F11%2F35W58PICzbv_1024.jpg', 'intro_src': 'http://img3.imgtn.bdimg.com/it/u=2358689708,1558820&fm=214&gp=0.jpg' },
+    ], //轮播数据列表
     winWidth: 0,  //设备宽度；
     winHeight: 0,   //设备高度；
 
-    itemWidth: 0, //单个轮播图swiper-item的宽度；
+    itemWidth:416, //单个轮播图swiper-item的宽度；
     itemHeight: 0,//单个轮播图swiper-item的高度；
     allWidth: 0,  //轮播展示 swiper的总宽度；
     scale: 0.7,   //  缩放大小倍数；
@@ -43,63 +50,78 @@ Page({
       distance = e.changedTouches[0].clientX - this.data.startClinetX; //距离间隔；
     //判断
     if (times < 500 && Math.abs(distance) > 10) {
-      let curIndex = this.data.curIndex;
-      let x0 = this.data.itemWidth, x1 = this.data.translateDistance, x = 0;
-      if (distance > 0) {
-        curIndex = curIndex - 1
+      let team_curIndex = this.data.team_curIndex;
+      let x0 = this.data.itemWidth*0.5, x1 = this.data.translateDistance, x = 0;
+      if (distance > 0) {     //向右滑
+        team_curIndex = team_curIndex - 1
         
-        if (this.data.prev > -1){
+        if (this.data.team_prev > -1){
           this.setData({
-            prev: this.data.prev - 1,
-            next: this.data.next - 1
+            team_prev: this.data.team_prev - 1,
+            team_next: this.data.team_next - 1
           })
-        } else if (this.data.prev <= -1) {
-          this.data.next = 1
-          this.data.prev = -1
+        } else if (this.data.team_prev <= -1) {
+          this.data.team_next = 1
+          this.data.team_prev = -1
         }
         
-        console.log(this.data.prev, this.data.next)
-        if (curIndex < 0) {
-          curIndex = 0;
+        console.log(this.data.team_prev, team_curIndex, this.data.team_next)
+        if (team_curIndex < 0) {
+          team_curIndex = 0;
           x0 = 0;
         }
-        x = x1 + x0;
-      } else {
-        if (this.data.prev < 3){
-          this.setData({
-            prev: this.data.prev + 1,
-            next: this.data.next + 1
-          })
-        } else if (this.data.prev >= 3) {
-          this.data.next = 5
-          this.data.prev = 3
+        if (team_curIndex==0){
+          x=315;
+          console.log('偏移300')
+        }else{
+          x = x1 + x0;
         }
         
-        curIndex = curIndex + 1
-        if (curIndex >= this.data.swiperList.length) {
-          curIndex = this.data.swiperList.length - 1;
+        console.log('向右滑之后：x=' + x+',x1='+x1)
+      } else {           //向左滑
+        if (this.data.team_prev < 3){
+          this.setData({
+            team_prev: this.data.team_prev + 1,
+            team_next: this.data.team_next + 1
+          })
+        } else if (this.data.team_prev >= 3) {
+          this.data.team_next = 5
+          this.data.team_prev = 3
+        }
+        
+        team_curIndex = team_curIndex + 1
+        if (team_curIndex >= this.data.swiperList.length) {
+          team_curIndex = this.data.swiperList.length - 1;
           x0 = 0;
-        }        
-        x = x1 - x0;
+        }
+        if (team_curIndex==1){
+          x = 208
+        } else {
+          x = x1 - x0;
+        }  
+            
+        
+        console.log('team_prev=' + this.data.team_prev + ',team_curIndex=' + team_curIndex + ',team_next=' + this.data.team_next)
+        console.log('向左滑之后：x=' + x + ',x1=' + x1)
       }
-      this.animationToLarge(curIndex, x);
-      this.animationToSmall(curIndex, x);
+      this.animationToLarge(team_curIndex, x);
+      this.animationToSmall(team_curIndex, x);
       this.setData({
-        curIndex: curIndex,
+        team_curIndex: team_curIndex,
         translateDistance: x
       })
 
     } 
   },
   // 动画
-  animationToLarge: function (curIndex, x) {
+  animationToLarge: function (team_curIndex, x) {
 
     this.animation.translateX(x).scale(1).step()
     this.setData({
       animationToLarge: this.animation.export()
     })
   },
-  animationToSmall: function (curIndex, x) {
+  animationToSmall: function (team_curIndex, x) {
 
     this.animation.translateX(x).scale(0.7).step()
     this.setData({
@@ -118,18 +140,18 @@ Page({
         that.setData({
           winWidth: w,
           winHeight: h,
-          itemWidth: w * 0.5,
+          // itemWidth: w * 0.5,
           allWidth: allWidth
         })
       },
     })
     this.animation = wx.createAnimation({
       transformOrigin: "50% 50%",
-      duration: 500,
+      duration: 800,
       timingFunction: "ease-out",
       delay: 0
     })
-    console.log(this.data.prev,this.data.curIndex,this.data.next)
+    console.log(this.data.team_prev, this.data.team_curIndex, this.data.team_next)
   },
 
 
